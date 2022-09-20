9768d8c3446f194852ed649ebc5c62baa2a9c358
Diego Garza <diego.garzam@uanl.edu.mx>
2022-09-20 09:58:35 -0500

    Agregando script de python desde Linux

---
+++ pokemon.py
@@ -0,0 +1,12 @@
+import requests
+
+if __name__='__main__':
+	url = 'http://pokeapi.co/api/v2/pokemon-form/'
+	response = request.get(url)
+	if response.status_code == 200:
+		payload = response.json()
+		results = payload.get('results',[])
+		if results:
+			for pokemon in results:
+				name = pokemon['name']
+				print(name)
